89f41cf5b0e2b7662414161d95389abd6941e3a7
unknown <vdamasco@github.com>
2019-09-15 17:43:28 -0300

    add new testAccount

---
+++ ParkingBusinessLogic/Account.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParkingBusinessLogic
+{
+    public class Account
+    {
+        public Boolean addBalance()
+        {
+            return true;
+        }
+
+        public String validateFormat(String num)
+        {
+            String result = "Formato Invalido";
+            num = num.Replace(" ","");
+
+            if ((num.Length == 9) || (num.Length == 8))
+            {
+                if (num.All(char.IsDigit))
+                {
+                    if((num.Length == 9) && (num[0] == '0') && (num[1]=='9') && (num[2]!='3'))
+                    {
+                        result = num.Insert(6," ") ;
+                        result = result.Insert(3, " ");
+                    } else if ((num[0]=='9') && (num[1]!='3') && (num.Length == 8))
+                    {
+                        result = num.Insert(5, " ");
+                        result = result.Insert(2, " ");
+                    }
+                }
+            }
+            return result;
+        }
+
+        public Boolean  enoughBalance ()
+        {
+            return true;
+        } 
+
+    }
+}
---
+++ TestParkingBusinessLogic/TestAccount.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ParkingBusinessLogic;
+
+namespace TestParkingBusinessLogic
+{
+    [TestClass]
+    public class TestAccount
+    {
+
+        Account myAccount;
+
+        [TestInitialize]
+        public void testInit()
+        {
+            myAccount = new Account();
+        }
+
+        [TestMethod]
+        public void testAddBalance()
+        {
+            Boolean output = myAccount.addBalance();
+
+            Assert.IsTrue(output);
+        }
+
+        [TestMethod]
+        public void testValidateFormatFalse1()
+        {
+            String num = "9f03uf134";
+            String expected = "Formato Invalido";
+            String output = myAccount.validateFormat(num);
+
+            Assert.AreEqual(output,expected);
+        }
+
+        [TestMethod]
+        public void testValidateFormatFalse2()
+        {
+            String num = "19042";
+            String expected = "Formato Invalido";
+            String output = myAccount.validateFormat(num);
+
+            Assert.AreEqual(output, expected);
+        }
+
+        [TestMethod]
+        public void testValidateFormatFalse3()
+        {
+            String num = "093457869";
+            String expected = "Formato Invalido";
+            String output = myAccount.validateFormat(num);
+
+            Assert.AreEqual(output, expected);
+        }
+
+        [TestMethod]
+        public void testValidateFormatFalse4()
+        {
+            String num = "0998473647875";
+            String expected = "Formato Invalido";
+            String output = myAccount.validateFormat(num);
+
+            Assert.AreEqual(output, expected);
+        }
+
+        [TestMethod]
+        public void testValidateFormatFalse5()
+        {
+            String num = "095 77 88 99 34";
+            String expected = "Formato Invalido";
+            String output = myAccount.validateFormat(num);
+
+            Assert.AreEqual(output, expected);
+        }
+
+        [TestMethod]
+        public void testValidateFormatFalse6()
+        {
+            String num = " 09577889934 ";
+            String expected = "Formato Invalido";
+            String output = myAccount.validateFormat(num);
+
+            Assert.AreEqual(output, expected);
+        }
+
+
+        [TestMethod]
+        public void testValidateFormatTrue1()
+        {
+            String num = "99673647";
+            String output = myAccount.validateFormat(num);
+            String expected = "99 673 647";
+
+            Assert.AreEqual(output, expected);
+        }
+
+        [TestMethod]
+        public void testValidateFormatTrue2()
+        {
+            String num = "099673647";
+            String output = myAccount.validateFormat(num);
+            String expected = "099 673 647";
+
+            Assert.AreEqual(output, expected);
+        }
+
+        [TestMethod]
+        public void testValidateFormatTrue3()
+        {
+            String num = "99 67 364 7";
+            String output = myAccount.validateFormat(num);
+            String expected = "99 673 647";
+
+            Assert.AreEqual(output, expected);
+        }
+        [TestMethod]
+        public void testValidateFormatTrue4()
+        {
+            String num = "   99673647  ";
+            String output = myAccount.validateFormat(num);
+            String expected = "99 673 647";
+
+            Assert.AreEqual(output, expected);
+        }
+        [TestMethod]
+        public void testValidateFormatTrue5()
+        {
+            String num = "09 96 73 6 47";
+            String output = myAccount.validateFormat(num);
+            String expected = "099 673 647";
+
+            Assert.AreEqual(output, expected);
+        }
+
+        [TestMethod]
+        public void testEnoughBalance()
+        {
+            Boolean output = myAccount.enoughBalance();
+
+            Assert.IsTrue(output);
+        }
+
+    }
+
+   
+}
