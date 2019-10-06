8044f01d9d8e162687e00d55f7c476e03dd959db
unknown <vdamasco@github.com>
2019-10-06 18:18:39 -0300

    fix a bug in Controller

--- TestParkingBusinessLogic/TestController.cs
+++ TestParkingBusinessLogic/TestController.cs
@@ -1,11 +1,9 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-using ParkingBusinessLogic;
-using System;
-using System.Collections.Generic;
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.Diagnostics.CodeAnalysis;
+using ParkingBusinessLogic;
+using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace TestParkingBusinessLogic
 {
@@ -13,25 +11,26 @@ namespace TestParkingBusinessLogic
     [TestClass]
     public class TestController
     {
-       
+        private Controller myController;
 
+        [TestInitialize]
+        public void InitTest()
+        {
+            myController = new Controller();
+        }
 
         [TestMethod]
         public void TestRegisterAccountOk()
         {
-            Controller myController = new Controller();
-            myController.InitLists();
             Account expected = new Account("098567890", 500);
             myController.RegisterAccount(expected);
             Account output = myController.Accounts.ElementAt(0);
-          
             Assert.AreEqual(output, expected);
         }
-       [TestMethod]
+
+        [TestMethod]
         public void TestFindAccountOk()
         {
-            Controller myController = new Controller();
-            myController.InitLists();
             Account wanted = new Account("098567890", 500);
             myController.RegisterAccount(wanted);
             Account expected = myController.FindAccount("098 567 890");
@@ -41,11 +40,9 @@ namespace TestParkingBusinessLogic
         [TestMethod]
         public void TestisAccountEmpty()
         {
-		Controller myController = new Controller();
-            myController.InitLists();
             Account myAccount = new Account();
             myController.RegisterAccount(myAccount);
-            bool  expected = myController.isAccountEmpty(myAccount);
+            bool expected = myController.isAccountEmpty(myAccount);
             Assert.IsTrue(expected);
 
         }
@@ -54,41 +51,19 @@ namespace TestParkingBusinessLogic
         [TestMethod]
         public void TestChangeValueOfMinute()
         {
-            Controller myController = new Controller();
-            myController.InitLists();
             int value = 22;
             myController.ChageValueMinute(value);
             int output = myController.ValueOfMinute.ValuePerMinutes;
             Assert.AreEqual(value, output);
         }
 
-        [TestMethod]
-
-        public void TestInitAccount()
-        {
-            Controller myController = new Controller();
-            myController.InitLists();
-            List<Account> EmptyAccount = myController.Accounts;
-            List<Account>  Accounts = new List<Account>();
-            Assert.AreEqual(EmptyAccount, Accounts);
-        }
-        [TestMethod]
-
-        public void TestInitPurchase()
-        {
-            Controller myController = new Controller();
-            myController.InitLists();
-            List<Purchase> EmptyPurchase = myController.Purchases;
-            List<Purchase> Purchases = new List<Purchase>();
-            Assert.AreEqual(EmptyPurchase, Purchases);
-        }
 
         [TestMethod]
         public void TestBuyParking()
         {
             Assert.IsTrue(true);
         }
-        
+
 
 
     }
