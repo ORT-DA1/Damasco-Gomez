be7b9ac62bd09f797e5a2e4e4a0f99219e48f22e
unknown <vdamasco@github.com>
2019-10-08 17:03:58 -0300

    Eliminate class Main, we dont need it

--- ParkingUserInterface/AddAccountInterface.cs
+++ ParkingUserInterface/AddAccountInterface.cs
@@ -55,7 +55,7 @@ namespace ParkingUserInterface
                 else if (balance.All(char.IsDigit))
                 {
                     MyController.AddBalanceInAccount(account, Int32.Parse(balance));
-                    
+                    GoToFirstInterface();
                 } 
                 else
                 {
@@ -78,6 +78,7 @@ namespace ParkingUserInterface
                 MyAccount = new Account(Number, 0);
                 MyController.RegisterAccount(MyAccount);
                 MessageBox.Show("Account created succesfully!");
+                GoToFirstInterface();
                 
             }
             else
