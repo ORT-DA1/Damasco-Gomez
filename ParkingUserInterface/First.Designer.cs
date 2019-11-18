2dce8ac02c347fbf386abf364c3d713134a786cd
Yuliana Gomez <yulianagomezsilva@gmail.com>
2019-11-18 18:17:41 -0300

    new flags ui

--- ParkingUserInterface/First.Designer.cs
+++ ParkingUserInterface/First.Designer.cs
@@ -28,17 +28,22 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(First));
             this.AddAccountButton = new System.Windows.Forms.Button();
             this.AddBalanceButton = new System.Windows.Forms.Button();
             this.CheckParkingButton = new System.Windows.Forms.Button();
             this.BuyParkingButton = new System.Windows.Forms.Button();
             this.CostParkingButton = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             this.SuspendLayout();
             // 
             // AddAccountButton
             // 
             this.AddAccountButton.Location = new System.Drawing.Point(186, 26);
-            this.AddAccountButton.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.AddAccountButton.Margin = new System.Windows.Forms.Padding(2);
             this.AddAccountButton.Name = "AddAccountButton";
             this.AddAccountButton.Size = new System.Drawing.Size(162, 35);
             this.AddAccountButton.TabIndex = 0;
@@ -49,7 +54,7 @@
             // AddBalanceButton
             // 
             this.AddBalanceButton.Location = new System.Drawing.Point(186, 82);
-            this.AddBalanceButton.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.AddBalanceButton.Margin = new System.Windows.Forms.Padding(2);
             this.AddBalanceButton.Name = "AddBalanceButton";
             this.AddBalanceButton.Size = new System.Drawing.Size(162, 35);
             this.AddBalanceButton.TabIndex = 1;
@@ -60,7 +65,7 @@
             // CheckParkingButton
             // 
             this.CheckParkingButton.Location = new System.Drawing.Point(186, 189);
-            this.CheckParkingButton.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.CheckParkingButton.Margin = new System.Windows.Forms.Padding(2);
             this.CheckParkingButton.Name = "CheckParkingButton";
             this.CheckParkingButton.Size = new System.Drawing.Size(162, 35);
             this.CheckParkingButton.TabIndex = 2;
@@ -71,7 +76,7 @@
             // BuyParkingButton
             // 
             this.BuyParkingButton.Location = new System.Drawing.Point(186, 136);
-            this.BuyParkingButton.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.BuyParkingButton.Margin = new System.Windows.Forms.Padding(2);
             this.BuyParkingButton.Name = "BuyParkingButton";
             this.BuyParkingButton.Size = new System.Drawing.Size(162, 35);
             this.BuyParkingButton.TabIndex = 3;
@@ -82,7 +87,7 @@
             // CostParkingButton
             // 
             this.CostParkingButton.Location = new System.Drawing.Point(186, 239);
-            this.CostParkingButton.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.CostParkingButton.Margin = new System.Windows.Forms.Padding(2);
             this.CostParkingButton.Name = "CostParkingButton";
             this.CostParkingButton.Size = new System.Drawing.Size(162, 35);
             this.CostParkingButton.TabIndex = 4;
@@ -90,19 +95,48 @@
             this.CostParkingButton.UseVisualStyleBackColor = true;
             this.CostParkingButton.Click += new System.EventHandler(this.CostParkingButton_Click);
             // 
+            // pictureBox1
+            // 
+            this.pictureBox1.AccessibleName = "ArgFlag";
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(3, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(113, 72);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.PictureBox1_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(419, 0);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(113, 72);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 6;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.PictureBox2_Click);
+            // 
             // First
             // 
+            this.AccessibleName = "UruFlag";
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
             this.ClientSize = new System.Drawing.Size(533, 288);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.CostParkingButton);
             this.Controls.Add(this.BuyParkingButton);
             this.Controls.Add(this.CheckParkingButton);
             this.Controls.Add(this.AddBalanceButton);
             this.Controls.Add(this.AddAccountButton);
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "First";
             this.Text = "Welcome!";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -114,6 +148,8 @@
         private System.Windows.Forms.Button CheckParkingButton;
         private System.Windows.Forms.Button BuyParkingButton;
         private System.Windows.Forms.Button CostParkingButton;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
     }
 }
 
